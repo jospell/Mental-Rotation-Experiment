969cea4a0ccee96a78132289a8448a78f70c18b5
[jspellmeyer] <[jspellmeyer@uni-osnabrueck.de]>
2019-05-13 18:37:18 +0200

    Changed 03_trials.js, 04_views.js and 05_main.js to create a mental rotation experiment

---
+++ 03_trials.js
@@ -0,0 +1,116 @@
+// In this file you can specify the trial data for your experiment
+
+const trial_info = {
+        key_press : [
+        {
+            question: "Are the two shown (3-diemensional) objects the same or are they different?",
+            picture: "mental_rotation_images/1_50_different.jpg",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different',
+            rotation: 50,
+            number_of_picture: 01
+        },
+        {
+            question: "Are the two shown (3-diemensional) objects the same or are they different?",
+            picture: "mental_rotation_images/1_50_same.jpg",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same',
+            rotation: 50,
+            number_of_picture: 02
+        },
+        {
+            question: "Are the two shown (3-diemensional) objects the same or are they different?",
+            picture: "mental_rotation_images/2_50_different.jpg",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different',
+            rotation: 50,
+            number_of_picture: 03
+        },
+        {
+            question: "Are the two shown (3-diemensional) objects the same or are they different?",
+            picture: "mental_rotation_images/2_50_same.jpg",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same',
+            rotation: 50,
+            number_of_picture: 04
+        },
+        {
+            question: "Are the two shown (3-diemensional) objects the same or are they different?",
+            picture: "mental_rotation_images/3_50_different.jpg",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different',
+            rotation: 50,
+            number_of_picture: 05
+        },
+        {
+            question: "Are the two shown (3-diemensional) objects the same or are they different?",
+            picture: "mental_rotation_images/3_50_same.jpg",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same',
+            rotation: 50,
+            number_of_picture: 06
+        },
+        {
+            question: "Are the two shown (3-diemensional) objects the same or are they different?",
+            picture: "mental_rotation_images/4_50_different.jpg",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different',
+            rotation: 50,
+            number_of_picture: 07
+        },
+        {
+            question: "Are the two shown (3-diemensional) objects the same or are they different?",
+            picture: "mental_rotation_images/4_50_same.jpg",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same',
+            rotation: 50,
+            number_of_picture: 08
+        },
+        {
+            question: "Are the two shown (3-diemensional) objects the same or are they different?",
+            picture: "mental_rotation_images/5_50_different.jpg",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'different',
+            rotation: 50,
+            number_of_picture: 09
+        },
+        {
+            question: "Are the two shown (3-diemensional) objects the same or are they different?",
+            picture: "mental_rotation_images/5_50_same.jpg",
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: 'different',
+            expected: 'same',
+            rotation: 50,
+            number_of_picture: 10
+        }
+    ]
+};
